e3b3da6b9857282166d50232868cf6bd6e742733
OnderKaratas <72624360+OnderKaratas@users.noreply.github.com>
2021-02-17 22:58:56 +0300

    njlkn

--- GameJam Game/Assets/Scripts/KarekterMovement.cs
+++ GameJam Game/Assets/Scripts/KarekterMovement.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class KarekterMovement : MonoBehaviour
 {
     public GameObject bullet;
+    public float moveSpeed;
     public Transform ateşnoktası;
     public float atışhızı;
     public float Sayaç;
+    Vector2 movement;
+    public Rigidbody2D rb;
+    public Vector2 JumpForce;
+    bool jump;
+
     private void Update()
     {
         Sayaç -= Time.deltaTime;
@@ -19,10 +25,31 @@ public class KarekterMovement : MonoBehaviour
                 Sayaç = 0.5f;
             }
         }
+        movement.x=Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            jump=true;
+        }
+        
     }
     public void shoot()
     {
         GameObject mermi = Instantiate(bullet, ateşnoktası.position, Quaternion.identity);
         mermi.GetComponent<Rigidbody2D>().velocity=new Vector2(atışhızı*Time.deltaTime,0);
     }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+movement*moveSpeed*Time.fixedDeltaTime);
+        ProcessJump();
+    }
+    private void ProcessJump()
+    {
+        if(jump)
+        {
+            rb.AddForce(JumpForce*Time.deltaTime, ForceMode2D.Force);
+            jump = false;
+        }
+
+    }
+    
 }
